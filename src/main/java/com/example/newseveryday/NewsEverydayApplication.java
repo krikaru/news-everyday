86313e12078ff8edec8eb3674489aac8a1c679ef
darya <lesoleill64@gmail.com>
2022-05-19 14:57:24 +0300

    Initial commit

---
+++ src/main/java/com/example/newseveryday/NewsEverydayApplication.java
@@ -0,0 +1,13 @@
+package com.example.newseveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsEverydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsEverydayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newseveryday/NewsEverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newseveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsEverydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
