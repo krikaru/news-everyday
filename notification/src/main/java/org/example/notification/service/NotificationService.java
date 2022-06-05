36784c2091610b40b5337e0cce51c6c00e737fa1
darya <lesoleill64@gmail.com>
2022-06-05 12:07:06 +0300

    fix

--- notification/src/main/java/org/example/notification/service/NotificationService.java
+++ notification/src/main/java/org/example/notification/service/NotificationService.java
@@ -2,6 +2,7 @@ package org.example.notification.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.notification.model.NotificationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+    @Autowired
     JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
