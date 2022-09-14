01e41176a61ad944f76af1dd9f04ef1932e65495
darya <lesoleill64@gmail.com>
2022-09-14 22:43:04 +0300

    optimize sql for get user

--- news-everyday-api/src/main/java/com/example/newsapi/repo/UserRepo.java
+++ news-everyday-api/src/main/java/com/example/newsapi/repo/UserRepo.java
@@ -1,6 +1,7 @@
 package com.example.newsapi.repo;
 
 import com.example.newsapi.model.AppUser;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<AppUser, Long> {
 
+    @EntityGraph(attributePaths = { "roles", "lockAccount" })
     Optional<AppUser> findByEmail(String email);
+
+    @Override
+    @EntityGraph(attributePaths = { "roles", "lockAccount" })
+    Optional<AppUser> findById(Long id);
 }
 
